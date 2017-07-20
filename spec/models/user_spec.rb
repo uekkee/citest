b6e4b3f8338534ec4f7cb946f2acf74b992cad73
UEKIHARA Susumu <uekkee@gmail.com>
2017-07-20 10:59:31 +0900

    count smaller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'sorting' do
-    let(:size) { 10000000 }
+    let(:size) { 1000 }
     let(:sorted) { [].tap { |a| size.times { |t| a << t } } }
     let(:shuffled) { sorted.shuffle }
     it { expect(sorted).to eq shuffled.sort }
   end
 
   context 'inserting' do
-    let(:size) { 1000000 }
+    let(:size) { 100 }
     before { create_list :user, size }
     it { expect(User.count).to eq size }
   end
